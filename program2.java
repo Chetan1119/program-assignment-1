ac0e2c534c2d9399ea5a10b31bae228f5b12e15c
GS1211 <manu174303@gmail.com>
2018-08-19 09:19:57 +0530

    Create program2.java
    print your name program

---
+++ program2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner
+public class Prog
+{
+public static void main(String[] a)
+{
+
+Scanner a=new Scanner(System.in);
+string b;
+b=a.nextLine;
+System.out.println("input your name");
+System.out.println(b);
+}
+
+
+}
